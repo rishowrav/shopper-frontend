c3e504f58eba707605651f771d299912e82d42d0
rishowrav <rishowrav@gmail.com>
2024-08-21 22:30:41 +0600

    login page design has been completed

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,5 +1,26 @@
+import "./CSS/LoginSignup.css";
+
 const LoginSignup = () => {
-  return <div></div>;
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder=" Password" />
+        </div>
+        <button type="submit">Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login</span>
+          <div className="loginsignup-agree">
+            <input type="checkbox" name="" id="" />
+            <p>By continuing, i agree to the terms fo use & privacy policy.</p>
+          </div>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSignup;
